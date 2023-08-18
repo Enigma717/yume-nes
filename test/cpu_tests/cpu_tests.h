4448b064edef5b2cf1d9a5df25c66fcf577901a5
Enigma717 <mt88x2@gmail.com>
2023-08-19 00:12:41 +0200

    Immediate opcodes behavioural tests

--- test/cpu_tests/cpu_tests.h
+++ test/cpu_tests/cpu_tests.h
@@ -4,13 +4,13 @@
 
 void ut_cpu_boot_and_reset();
 void ut_cpu_bus_memory_connection();
-void ut_cpu_immediate_opcodes_behaviour();
-// void ut_cpu_immediate_opcodes_flags();
+void ut_cpu_status_register();
+void ut_cpu_lookup_table_correctness();
 void ut_cpu_implied_opcodes_behaviour();
 void ut_cpu_implied_opcodes_flags();
-void ut_cpu_lookup_table_correctness();
+void ut_cpu_immediate_opcodes_behaviour();
+// void ut_cpu_immediate_opcodes_flags();
 void ut_cpu_perform_cycle_correctness();
-void ut_cpu_status_register();
 
 
 #endif
---
+++ test/cpu_tests/ut_cpu_immediate_opcodes_behaviour.cpp
@@ -0,0 +1,198 @@
+#include "./cpu_tests.h"
+
+#include "../test_main.h"
+#include "../../include/bus.h"
+#include <cstdint>
+
+
+namespace InstrLookup
+{
+    using MN = Instruction::MnemonicName;
+    using AM = Instruction::AddressingMode;
+
+    constexpr Instruction brk_instr {MN::BRK, AM::implied, 0x00, 1, 7};
+}
+
+void test_adc_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_result {0x9B};
+    SystemMemory program_code {0xA9, 0x73, 0x69, 0x28, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.acc == target_result);
+}
+
+void test_and_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_result {0x22};
+    SystemMemory program_code {0xA9, 0x33, 0x29, 0xAA, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.acc == target_result);
+}
+
+void test_cmp_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_word {0x37};
+    SystemMemory program_code {0xA9, 0x77, 0xC9, 0x77, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.status.word == target_word);
+}
+
+void test_cpx_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_word {0x37};
+    SystemMemory program_code {0xA2, 0x77, 0xE0, 0x77, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.status.word == target_word);
+}
+
+void test_cpy_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_word {0x37};
+    SystemMemory program_code {0xA0, 0x77, 0xC0, 0x77, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.status.word == target_word);
+}
+
+void test_eor_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_result {0x99};
+    SystemMemory program_code {0xA9, 0x33, 0x49, 0xAA, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.acc == target_result);
+}
+
+void test_lda_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_value {0xAE};
+    SystemMemory program_code {0xA9, 0xAE, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.acc == target_value);
+}
+
+void test_ldx_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_value {0xCB};
+    SystemMemory program_code {0xA2, 0xCB, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.x_reg == target_value);
+}
+
+void test_ldy_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_value {0xEA};
+    SystemMemory program_code {0xA0, 0xEA, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.y_reg == target_value);
+}
+
+void test_ora_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_result {0xBB};
+    SystemMemory program_code {0xA9, 0x33, 0x09, 0xAA, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.acc == target_result);
+}
+
+void test_sbc_imm_behaviour()
+{
+    SystemBus bus;
+    uint8_t target_result {0x4A};
+    SystemMemory program_code {0xA9, 0x73, 0xE9, 0x28, 0x00};
+
+    bus.ram->memory_load_program(program_code, bus.cpu.pc);
+
+    do {
+        bus.cpu.perform_cycle();
+    } while (!(bus.cpu.curr_instruction == InstrLookup::brk_instr));
+
+    MY_ASSERT(bus.cpu.acc == target_result);
+}
+
+
+
+void ut_cpu_immediate_opcodes_behaviour()
+{
+    TEST_SET;
+
+    test_adc_imm_behaviour();
+    test_and_imm_behaviour();
+    test_cmp_imm_behaviour();
+    test_cpx_imm_behaviour();
+    test_cpy_imm_behaviour();
+    test_eor_imm_behaviour();
+    test_lda_imm_behaviour();
+    test_ldx_imm_behaviour();
+    test_ldy_imm_behaviour();
+    test_ora_imm_behaviour();
+    test_sbc_imm_behaviour();
+}
