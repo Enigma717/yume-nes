e3aaf330016eeb427c433e57fdae605f90b35cbd
Enigma717 <mt88x2@gmail.com>
2023-09-05 19:41:17 +0200

    Check logo correctness in header

--- include/cartridge.h
+++ include/cartridge.h
@@ -6,22 +6,32 @@
 #include <string>
 #include <vector>
 
+using CartridgeContents = std::vector<uint8_t>;
 
 namespace CartridgeConsts
 {
-    constexpr size_t header_size {16};
-}
+    constexpr size_t header_size  {16};
+    constexpr size_t prg_rom_size {16384};
+    constexpr size_t chr_rom_size {8192};
 
-using CartridgeContents = std::vector<uint8_t>;
+    const CartridgeContents nes_logo {0x4E, 0x45, 0x53, 0x1A};
+}
 
 
 class Cartridge {
 public:
+    CartridgeContents cartridge_dump {};
+    CartridgeContents header {CartridgeContents(CartridgeConsts::header_size, 0x00)};
+
+    int prg_rom_banks_count {0};
+    int chr_rom_banks_count {0};
+
+
     void dump_cartridge_into_vector(const std::string &cartridge_path);
     void load_header();
 
-    CartridgeContents cartridge_dump {};
-    CartridgeContents header {CartridgeContents(CartridgeConsts::header_size, 0x00)};
+    bool check_nes_logo_in_header();
+    bool decode_header();
 };
 
 
--- src/cartridge.cpp
+++ src/cartridge.cpp
@@ -8,28 +8,47 @@
 
 void Cartridge::dump_cartridge_into_vector(const std::string &cartridge_path)
 {
-    if(std::filesystem::exists(cartridge_path))
-        std::cout << "LOADED FILE: " << cartridge_path << "\n";
+    if(std::filesystem::exists(cartridge_path)) {
+        std::cout << "Loaded cartridge: " << cartridge_path << "\n";
+    }
+    else {
+        std::cout << "Cartridge has not been loaded\n";
+        return;
+    }
 
     std::ifstream game(cartridge_path, std::ios::in | std::ios::binary);
-
     game.unsetf(std::ios::skipws);
 
     game.seekg(0, std::ios::end);
     std::streampos cartridge_size = game.tellg();
     game.seekg(0, std::ios::beg);
 
-    std::cout << "SIZE: " << cartridge_size << "\n";
+    std::cout << "Cartridge size: " << cartridge_size << " bytes\n";
 
     cartridge_dump.reserve(cartridge_size);
-
     cartridge_dump.insert(cartridge_dump.begin(),
         std::istream_iterator<uint8_t>(game),
         std::istream_iterator<uint8_t>());
 }
 
-
 void Cartridge::load_header()
 {
     std::copy(cartridge_dump.begin(), cartridge_dump.begin() + CartridgeConsts::header_size, header.begin());
+}
+
+bool Cartridge::check_nes_logo_in_header()
+{
+    CartridgeContents nes_logo_in_header {header.begin(), header.begin() + CartridgeConsts::nes_logo.size()};
+
+    return nes_logo_in_header == CartridgeConsts::nes_logo;
+}
+
+bool Cartridge::decode_header()
+{
+    if (check_nes_logo_in_header() == false) {
+        std::cout << "Nes logo in cartridge is not correct\n";
+        return false;
+    }
+
+    return true;
 }
