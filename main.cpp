c3801dbca71b332589ac42434600dacfbba5fce4
Enigma717 <mt88x2@gmail.com>
2023-10-21 13:52:53 +0200

    Try rendering screen pixel by pixel

--- main.cpp
+++ main.cpp
@@ -15,7 +15,8 @@ namespace
     constexpr auto scale {1};
     constexpr auto screen_width {256 * scale};
     constexpr auto screen_height {240 * scale};
-    constexpr auto square_size {16 * scale};
+    constexpr auto square_size {8 * scale};
+    constexpr auto pixel_size {1 * scale};
 }
 
 
@@ -38,18 +39,16 @@ int main()
 
     auto color_index {0x00};
 
-    for (auto i {0}; i < 16; i++)
-    {
-        for (auto j {0}; j < 16; j++)
-        {
+    for (auto i {0}; i < (screen_height / scale); i++) {
+        for (auto j {0}; j < (screen_width / scale); j++) {
             if (color_index > 0x3F)
                 color_index = 0x00;
 
-            const auto x_coord = static_cast<float>(j * square_size);
-            const auto y_coord = static_cast<float>(i * square_size);
+            const auto x_coord = static_cast<float>(j * pixel_size);
+            const auto y_coord = static_cast<float>(i * pixel_size);
             const auto current_color {PPUColors::available_colors[color_index]};
 
-            sf::RectangleShape square(sf::Vector2f(square_size, square_size));
+            sf::RectangleShape square(sf::Vector2f(pixel_size, pixel_size));
             square.setFillColor(current_color);
             square.setPosition(x_coord, y_coord);
 
@@ -59,14 +58,11 @@ int main()
         }
     }
 
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed ||
-                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape))
-            {
+                (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)) {
                 window.close();
                 return 0;
             }
@@ -74,8 +70,17 @@ int main()
 
         window.clear();
 
-        for (const auto& square : squares) {
+        for (auto i {0}; i < squares.size(); i++) {
+            const auto& square {squares[i]};
             window.draw(square);
+
+            // sf::Font font;
+            // font.loadFromFile("/usr/share/fonts/TTF/iosevka-medium.ttc");
+            // auto text = sf::Text{std::to_string(i), font, 10};
+            // text.setPosition((square.getPosition().x + square.getSize().x / 2) - (text.getLocalBounds().width / 2),
+            // (square.getPosition().y + square.getSize().y / 2) - (text.getLocalBounds().height / 2));
+
+            // window.draw(text);
         }
 
         window.display();
